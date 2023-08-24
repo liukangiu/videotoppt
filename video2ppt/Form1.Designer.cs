53ab66c9b3134f4aa9913d80525a97a7be804b0a
刘康 <71562103+liukangiu@users.noreply.github.com>
2023-08-24 22:16:08 +0800

    Add files via upload

---
+++ video2ppt/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace video2ppt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Lable1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // Lable1
+            // 
+            resources.ApplyResources(this.Lable1, "Lable1");
+            this.Lable1.Name = "Lable1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // textBox9
+            // 
+            resources.ApplyResources(this.textBox9, "textBox9");
+            this.textBox9.Name = "textBox9";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // textBox10
+            // 
+            resources.ApplyResources(this.textBox10, "textBox10");
+            this.textBox10.Name = "textBox10";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // textBox11
+            // 
+            resources.ApplyResources(this.textBox11, "textBox11");
+            this.textBox11.Name = "textBox11";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Lable1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Lable1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ video2ppt/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CTW_APIS_shp;
+
+namespace video2ppt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const string core_labs_dir = "CORE.dll";
+
+        [DllImport(core_labs_dir, EntryPoint = "core_startwork", CallingConvention = CallingConvention.StdCall)]//从非托管DLL中导出函数
+        private static extern int core_startwork(double invfps, double prob, char[] vpd,int vpdlen, char[] imgpath,int ipdlen, int startusesecond, int endusesecond, IntPtr outinf_ptr);
+
+        public static int readfile_tan(out string filepath)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Mp4|*.mp4";
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                filepath = openFileDialog.FileName;
+                return 0;
+            }
+            else
+            {
+                filepath = "ERROR:读取错误";
+                return 1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "请选择⽂件夹";
+            //folderBrowserDialog1.ShowNewFolderButton = true; 
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mp4path;
+            readfile_tan(out mp4path);
+            textBox1.Text = mp4path;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            double invfps = 1;//抽帧间隔,单位秒，数值越大处理时间越短
+            double prob = 0.9;//图像相似度
+
+            string video_path = textBox1.Text;//视频路径
+            string imgpath = textBox3.Text;//文件夹路径
+
+            int starthour = Convert.ToInt32(textBox4.Text);//开始时间       
+            int startmin = Convert.ToInt32(textBox5.Text);
+            int startsecond = Convert.ToInt32(textBox6.Text);
+
+            int endhour = Convert.ToInt32(textBox9.Text);
+            int endmin = Convert.ToInt32(textBox8.Text);//结束时间 
+            int endsecond = Convert.ToInt32(textBox7.Text);
+
+            int startusesecond = (starthour * 60 + startmin) * 60 + startsecond;
+            int endusesecond = (endhour * 60 + endmin) * 60 + endsecond;
+
+
+
+
+            char[] outinf = new char[50];
+            for (int i = 0; i < 20; i++)
+            {
+                outinf[i] = '0';
+            }
+
+            //int l=outinf.GetLength(0);
+            IntPtr outinf_ptr = Marshal.AllocHGlobal(50);
+            Marshal.Copy(outinf, 0, outinf_ptr, 50);
+
+            //double invfps = 1;//抽帧间隔,单位秒，数值越大处理时间越短
+            //double prob = 0.9;//图像相似度
+
+            //string video_path = "D:\\桌面\\1\\f100040.mp4";//视频路径
+            //string imgpath = "D:\\桌面\\2";//文件夹路径
+
+            //int startusesecond = (0 * 60 + 25) * 60 + 35;
+            //int endusesecond = (2 * 60 + 17) * 60 + 22;
+
+
+            int jindu = 0;
+
+            int recode = ctw_apis_model_shp.cs_startwork(invfps, prob, video_path, imgpath, startusesecond, endusesecond, outinf_ptr);
+
+            byte[] infor = new byte[50];
+            byte b;
+            char c;
+            //progressBar1.Value = 10;
+            while (true)
+            {
+                Marshal.Copy(outinf_ptr, infor, 0, 2);
+                b = infor[0];
+                //Console.Write((int)b);
+                progressBar1.Value = (int)b;
+                if(b>=99)
+                {
+                    textBox2.Text = "处理完成，请打开文件夹查看！";
+                    break;
+                }
+                Thread.Sleep(2000);
+            }
+
+        }
+
+        private int startfun()
+        {
+            byte[] outinf = new byte[20];
+            for (int i = 0; i < 20; i++)
+            {
+                outinf[i] = 0;
+            }
+            IntPtr outinf_ptr = Marshal.AllocHGlobal(20);
+            Marshal.Copy(outinf, 0, outinf_ptr, 20);
+
+            if (check() == 0) return 1;
+
+            double invfps = 1;//抽帧间隔,单位秒，数值越大处理时间越短
+            double prob = 0.9;//图像相似度
+
+            string video_path = textBox1.Text;//视频路径
+            string imgpath = textBox3.Text;//文件夹路径
+
+            int starthour = Convert.ToInt32(textBox4.Text);//开始时间       
+            int startmin = Convert.ToInt32(textBox5.Text);
+            int startsecond = Convert.ToInt32(textBox6.Text);
+
+            int endhour = Convert.ToInt32(textBox9.Text);
+            int endmin = Convert.ToInt32(textBox8.Text);//结束时间 
+            int endsecond = Convert.ToInt32(textBox7.Text);
+
+            int startusesecond = (starthour * 60 + startmin) * 60 + startsecond;
+            int endusesecond = (endhour * 60 + endmin) * 60 + endsecond;
+            int jindu=0;
+
+
+            int vpathlen = video_path.Length;
+            int vpathlenadd = 0;
+            for (int i = 0; i < vpathlen; i++)
+            {
+                if ((int)video_path[i] > 256)
+                {
+                    vpathlenadd += 1;
+                }
+            }
+
+            int imgpathlen = imgpath.Length;
+            int impathlenadd = 0;
+            for (int i = 0; i < imgpathlen; i++)
+            {
+                if ((int)imgpath[i] > 256)
+                {
+                    impathlenadd += 1;
+                }
+            }
+
+            vpathlen += vpathlenadd;
+            imgpathlen += impathlenadd;
+            char[] vpd = new char[vpathlen];
+            char[] ipd = new char[imgpathlen];
+
+            vpd = video_path.ToCharArray();
+            ipd = imgpath.ToCharArray();
+
+
+            startwork(invfps, prob, vpd, vpathlen, ipd, imgpathlen, startusesecond, endusesecond, outinf_ptr);
+
+            while (true)
+            {
+                
+                if (jindu!= outinf[0])
+                {
+                    jindu = outinf[0];
+                    progressBar1.Value = jindu;
+                } 
+                if(outinf[1] == 1)
+                {
+                    break;
+                }
+            }
+            return 1;
+        }
+        private int check()
+        {
+            string video_path = textBox1.Text;//视频路径
+            string imgpath = textBox3.Text;//文件夹路径
+
+            int starthour = Convert.ToInt32(textBox4.Text) ;//开始时间       
+            int startmin = Convert.ToInt32(textBox5.Text) ;
+            int startsecond = Convert.ToInt32(textBox6.Text);
+
+            int endhour = Convert.ToInt32(textBox9.Text);
+            int endmin = Convert.ToInt32(textBox8.Text);//结束时间 
+            int endsecond = Convert.ToInt32(textBox7.Text); 
+
+            int startusesecond = (starthour * 60 + startmin) * 60 + startsecond;
+            int endusesecond = (endhour * 60 + endmin) * 60 + endsecond;
+
+            if (video_path == null || imgpath == null)
+            {
+                MessageBox.Show("ERROR:视频或文件夹路径为空!");
+                return 0;
+
+            }
+            if (startusesecond > endusesecond || endusesecond == 0)
+            {
+                MessageBox.Show("ERROR:分割时间段选择错误!");
+                return 0;
+            }
+
+            //startwork( invfps, prob, video_path, imgpath, startusesecond, endusesecond,out outinform, out jindu);
+            return 1;
+        }
+
+        private int startwork(double invfps, double prob, char[] vpd, int vpdlen, char[] ipd, int ipdlen, int startusesecond, int endusesecond,IntPtr outinf_ptr)
+        {
+
+            core_startwork(invfps, prob, vpd, vpdlen, ipd ,ipdlen, startusesecond, endusesecond, outinf_ptr);
+            return 1;
+        }
+    }
+}
